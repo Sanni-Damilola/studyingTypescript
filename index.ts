2102d8fbe0c7ab5d79edee20156e9ae8404acaa9
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-12 17:40:31 +0100

    update

--- index.ts
+++ index.ts
@@ -3,4 +3,6 @@ let id: number = 5;
 let myName: string = "sanni";
 let healthAndWealth: boolean = true;
 
-
+// array
+let ids: number[] = [1, 2, 4, 5, 6, 7]; // can only accept number data type in the array
+let arr: any[] = [1, false, "string", { name: "tolu" }, ["cooking", "eating"]]; // type an can accept any data type in an array
