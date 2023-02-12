36cfa7a47598a4e528998a43a7c7956333e567c3
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-12 17:42:43 +0100

    update

--- index.ts
+++ index.ts
@@ -6,3 +6,6 @@ let healthAndWealth: boolean = true;
 // array
 let ids: number[] = [1, 2, 4, 5, 6, 7]; // can only accept number data type in the array
 let arr: any[] = [1, false, "string", { name: "tolu" }, ["cooking", "eating"]]; // type an can accept any data type in an array
+
+// Tupple
+let person: [number, string, boolean] = [1, "sanni", true]; // setting the data type in the array in this paticular order depending on the types you set
