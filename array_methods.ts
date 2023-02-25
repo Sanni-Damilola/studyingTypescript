e3e411e12e285d17e5973de6f57afb4a2cd0b680
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-25 13:24:50 +0100

    update

---
+++ array_methods.ts
@@ -0,0 +1,60 @@
+// repraticing array methods
+
+/* 
+copywithin
+entries 
+every 
+fill
+filter
+find 
+findindex 
+flat 
+flatMap 
+forEach 
+includes 
+indexOf 
+join 
+keys 
+lastindexOf
+map 
+pop
+push
+reduce
+reduceRight
+reverse
+shift
+slice
+some
+sort
+splice
+toLocalString
+toString
+unshift
+values
+*/
+
+const data = [
+  {
+    name: "sanni",
+    id: 1,
+    status: true,
+  },
+  {
+    name: "bola",
+    id: 2,
+    status: false,
+  },
+  {
+    name: "lola",
+    id: 3,
+    status: false,
+  },
+  {
+    name: "abu",
+    id: 4,
+    status: true,
+  },
+];
+
+let copywithin = data.copyWithin();
+console.log(copywithin);
