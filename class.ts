6b63d6a52731801f94960496f73b3bf2a8906635
Sanni-Damilola <sannifortune11@gmail.com>
2024-01-05 13:01:45 +0100

    added

--- class.ts
+++ class.ts
@@ -5,7 +5,7 @@ class Person {
 const person = new Person();
 person.name = "Jane";
 
-console.log(person);
+// console.log(person);
 
 class Person1 {
   private name: string;
@@ -18,3 +18,6 @@ class Person1 {
     return this.name;
   }
 }
+
+const person1 = new Person1("Jane");
+// console.log(person1);
